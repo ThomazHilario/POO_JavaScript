3e309487652fcc05fc777adc7f45a7cad093d11d
Thomaz Alves <thomazhilario5@gmail.com>
2023-06-19 18:24:22 -0300

    objeto global

---
+++ Aula 11-Objeto global window/index.js
@@ -0,0 +1,8 @@
+/* Objeto globa window */
+// Sempre que é iniciada um página web traz o objeto window.
+// As variáveis globais são atreladas ao window.
+// this no escopa global se refere ao window
+
+
+console.log(window)
+console.log(this)
