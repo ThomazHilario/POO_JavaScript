d623f30c3971bca249f6974e0b4d2cad90a733bc
Thomaz Alves <thomazhilario5@gmail.com>
2023-06-21 11:27:56 -0300

    Object Literals

---
+++ Aula 16-Object Literals/index.js
@@ -0,0 +1,13 @@
+/* Object Literals */
+// Função do ES6, que premite criar objetos mais rápido.
+// Aproveita o nome das variáveis como chave, e o valor da variável como valor do objeto.
+
+let name = 'thomaz'
+let age = 18
+
+let person = {
+    name,
+    age
+}
+
+console.log(person)
