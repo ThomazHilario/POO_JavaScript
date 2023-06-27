e430ce16cb7741cee8914bf147df28575f74f18f
Thomaz Alves <thomazhilario5@gmail.com>
2023-06-27 18:30:51 -0300

    Exercicio 03 - function construct

---
+++ Exercicios/Exercicio 03/index.js
@@ -0,0 +1,13 @@
+// Crie um objeto ninja, por function construct.
+// Com propriedade name e método atirarShuriken()
+
+function createNinja(name,atirarShuriken){
+    this.name = name
+    this.atirarShuriken = function(){
+        console.log(`${this.name} Atirou a shuriken`)
+    }
+}
+
+let ninja = new createNinja('Naruto')
+
+ninja.atirarShuriken()
