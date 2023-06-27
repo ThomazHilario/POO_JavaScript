4ee6a2aed3732d02af5b0cc63e1fa027b59eb454
Thomaz Alves <thomazhilario5@gmail.com>
2023-06-27 17:52:03 -0300

    Exercicio 01

---
+++ Exercicios/Exercicio 01/index.js
@@ -0,0 +1,12 @@
+// Crie um objeto com 3 propriedades.
+// A primeira deve ser uma string, a segunda deve ser um number e a terceira um boolean.
+// Por ultimo dê um console.log() em todas as propriedades.
+
+
+const person ={
+    name:'Thomaz',
+    age:18,
+    usaOculos:false
+}
+
+console.log(person.name + '\n' + person.age + '\n' + person.usaOculos)
