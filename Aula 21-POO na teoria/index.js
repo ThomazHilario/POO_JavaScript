b43e741fe43a1668c24ddf95724dab53d5bc5eae
Thomaz Alves <thomazhilario5@gmail.com>
2023-07-03 15:33:15 -0300

    POO na teoria

---
+++ Aula 21-POO na teoria/index.js
@@ -0,0 +1,16 @@
+
+/* 
+    -> Objetos
+
+    - Representação de uma classe.
+    - Possui propriedades: características.
+    -- Possui métodos: Ações do objeto.
+
+
+    -> Classe
+
+    - Molde de um objeto.
+    - Geralmente se criam vários objetos da mesma classe.
+    - normalmente ja possui propriedades e métodos.
+    - No javascript foi implementada no ES6.
+*/
