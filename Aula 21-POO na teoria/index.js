5d3992144783e3a90a01fb3f628c6d307ad6b447
Thomaz Alves <thomazhilario5@gmail.com>
2023-07-04 11:39:24 -0300

    Poo: teoria parte 2

--- Aula 21-POO na teoria/index.js
+++ Aula 21-POO na teoria/index.js
@@ -13,4 +13,16 @@
     - Geralmente se criam vários objetos da mesma classe.
     - normalmente ja possui propriedades e métodos.
     - No javascript foi implementada no ES6.
+
+    -> Encapsulação
+
+    - Quando o objeto encapsula dados ou meios de fazer algo com os dados(Usando métodos).
+    - A encapsulação pode ter propriedades e métodos: Públicos, privados e protegidos.
+    - No javaScript os dados são públicos, porem podemos mudar isto.
+
+    -> Agregação
+
+    - Decompor um objeto grande em um objeto menor.
+    - objeto grande = SalaDeAula.
+    - objeto com agreggation = SalaDeAula com Aluno, Mesa, Lousa
 */
