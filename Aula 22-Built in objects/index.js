b91d114ee6bf9b9fc1725edcf7e2a00576c4f89c
Thomaz Alves <thomazhilario5@gmail.com>
2023-07-10 10:49:22 -0300

    Built in objects

---
+++ Aula 22-Built in objects/index.js
@@ -0,0 +1,6 @@
+/*
+    -> built in objects
+    - Objetos que vem do javascript
+    - São metodos e propriedades que ja tem nativamente para cada tipo de dado no javascript.(Prototypes)
+    - Ao criarmos uma variável com qualquer tipo de dado,esta mesma ja é um objeto possuindo suas propriedades e métodos.
+*/
