40ac5e69aece53dd7d830800cd7177c764b2c6f2
Thomaz Alves <thomazhilario5@gmail.com>
2023-07-13 10:20:44 -0300

    Métodos Math

---
+++ Aula 24-Math/index.js
@@ -0,0 +1,24 @@
+/*
+    -> Math
+    - Objeto com métodos para resoluções matemáticas
+    - math.Floor() -> arredonda um número para baixo
+    - Math.round() -> arredonda para um número mais proximo
+    - Math.ceil() -> arredonda para um número acima
+    - Math.random() -> Gera números aleatórios
+*/
+
+// Usando o Math.random() para gerar número ate um certo valor
+
+console.log(Math.floor(Math.random() * 11))
+
+// Usando o Math.floor()
+
+console.log(Math.floor(14.55))
+
+// Usando o Math.round()
+
+console.log(Math.round(13.35))
+
+// Usando o Math.ceil
+
+console.log(Math.ceil(14.50))
