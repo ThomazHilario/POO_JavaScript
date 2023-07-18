a540ec47db3bc1694bfb00a5bb2cfebae12ab5ec
Thomaz Alves <thomazhilario5@gmail.com>
2023-07-18 11:06:44 -0300

    Implementando a lógica na calculadora

--- Projetos/Calculadora/js/main.js
+++ Projetos/Calculadora/js/main.js
@@ -27,17 +27,101 @@ class Calculator {
         calculator.resultValue.textContent = 0 
     }
 
+    // Métodos das operações
+
+    sum(n1,n2){
+        return parseFloat(n1) + parseFloat(n2)
+    }
+
+    sub(n1,n2){
+        return parseFloat(n1) - parseFloat(n2)
+    }
+
+    multiplication(n1,n2){
+        return parseFloat(n1) * parseFloat(n2)
+    }
+
+    division(n1,n2){
+        return parseFloat(n1) / parseFloat(n2)
+    }
+
+    resolution(){
+        // Array
+        let array = (calculator.upperValue.textContent).split(' ')
+        let result = 0
+        // Resultado da operação
+
+       for(let idx = 0; idx < array.length;idx++){
+            let operation = 0
+            
+            // Verificando se result tem valor
+
+            if(result){
+                this.reset = 1
+            }
+
+            // Multiplicação
+            if(array[idx] == 'X'){
+                result = calculator.multiplication(array[idx - 1],array[idx + 1])
+                this.upperValue.textContent = result
+                this.resultValue.textContent = result
+                operation = 1
+            } else if(array[idx] == '/'){
+                // Divisão
+                result = calculator.division(array[idx - 1],array[idx + 1])
+                this.upperValue.textContent = result
+                this.resultValue.textContent = result
+                operation = 1
+            } else if(!array.includes('X') && !array.includes('/')){
+                // Soma
+                if(array[idx] == '+'){
+                    result = calculator.sum(array[idx - 1],array[idx + 1])
+                    this.upperValue.textContent = result
+                    this.resultValue.textContent = result
+                    operation = 1
+                } else if(array[idx] == '-'){
+                    // Subtração
+                    result = calculator.sub(array[idx - 1],array[idx + 1])
+                    this.upperValue.textContent = result
+                    this.resultValue.textContent = result
+                    operation = 1
+                }
+            }
+
+            // Condição tenha masi de uma operação a ser feita n acalculadora
+
+            if(operation){
+                // A primeira posição do rray recebe o valor do result
+                array[idx - 1] = result
+                // limpar os valores ja utilizados
+                array.splice(idx,2)
+                // Atualiza o indice      
+                idx = 0
+            }
+        }
+    }
+
     btnPress(){
 
         let input = this.textContent
         let upperValue = calculator.upperValue.textContent
 
-        // Verificando se o usuário apertou um número
+        // variável com expressão de verificação numérica
+
         let verify = new RegExp('^\\d+$')
 
+        if(calculator.reset && verify.test(input)){
+            upperValue = 0
+        }
+
+        calculator.reset = 0
+
+        // Condição caso o valor do botão seja AC
 
         if(input == 'AC'){
             calculator.removeCharactersInScreen()
+        }else if(input == '='){
+            calculator.resolution()
         }else{
 
             if(calculator.checkpress(input,upperValue,verify)){
