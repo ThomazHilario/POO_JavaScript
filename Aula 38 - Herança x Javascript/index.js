6d2680c9abd0ac8fb756389b41687a21428513fe
Thomaz Alves <thomazhilario5@gmail.com>
2023-07-25 12:50:52 -0300

    Herança em javascript

---
+++ Aula 38 - Herança x Javascript/index.js
@@ -0,0 +1,7 @@
+/*
+    -> Herança x JavaScript
+    - Herança vai auxiliar na reutilização do código
+    - Podemos pensar em uma classe veiculo que tem objetos filhos como carro,Moto,caminhão. No qual ambos são veiculos e compartilham métodos e propriedades do pai(veículos).
+    - Como destacado a pouco a classes dos filhos irão herdar métodos e propriedades da classe pai.
+    - No Javascript a herança pode ser aplicada via prototype CHAIN.
+*/
