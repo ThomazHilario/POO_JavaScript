409a41401084370a265cc60eadd13018f21351a9
Thomaz Alves <thomazhilario5@gmail.com>
2023-07-26 11:05:44 -0300

    Cuidados com o side effect

--- Aula 41-Copiando o prototype/index.js
+++ Aula 41-Copiando o prototype/index.js
@@ -1,6 +1,7 @@
 /*
     -> Podemos copiar um prototype
     - Ao inves de instanciar um novo objeto passando os seus métodos e atributos para outra classe, podemos copiar o prototype do pai e colocar na classe filha.
+    - Ao clonar um prototype é necessário observar se os seus métodos e propriedades não vão mudar, pois estamo passando a referência de uma classe para outra.
 */
 
 function Pessoa(){}
