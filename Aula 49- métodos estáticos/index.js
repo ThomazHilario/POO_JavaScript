ec5859021d4d82f8711912f9a0d9ccb0693a9e01
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-02 10:10:23 -0300

    métodos estáticos

---
+++ Aula 49- métodos estáticos/index.js
@@ -0,0 +1,13 @@
+/*
+    -> métodos estáticos
+    - métodos que só funcionam caso você utilize o nome da classe.
+    - Utilizado muito como helper
+*/
+
+class Helper{
+    static sum(a,b){
+        return a * b
+    }
+}
+
+console.log(Helper.sum(5,5))
