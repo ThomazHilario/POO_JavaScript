a42d7f733eac675292c6e820093966acf5eb82dc
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-03 18:52:08 -0300

    Adicionando  media Query

--- Projetos/To do List/js/main.js
+++ Projetos/To do List/js/main.js
@@ -2,12 +2,27 @@ class List{
     constructor(){
         this.input = document.getElementById('iAdd')
         this.ul = document.querySelector('ul')
+        this.msg = document.getElementById('msg')
+        this.totalTaks = 0
     }
-    deletetask(){
+    deleteTask(){
         let li = this.parentElement
         li.remove()
+        // Função que ativará a mensagem caso não tenha tarefas
+        to_Do_List.totalTaks--
+        to_Do_List.displayMsg()
+    }
+    completeTask(){
+        let li = this.parentElement
+
+        if(li.style.background == 'rgb(130, 73, 187)'){
+            li.style.background = 'rgba(0, 0, 0, 0)'
+        } else{
+            li.style.background = 'rgb(130, 73, 187)'
+        }
     }
     btnClick(input){
+        to_Do_List.totalTaks++
         // Criando elemento li
         let li = document.createElement('li')
         // Modificando o conteúdo interno html do li
@@ -17,7 +32,22 @@ class List{
 
         // Capturando icon de remover
         let deletebtn = li.lastElementChild
-        deletebtn.addEventListener('click',to_Do_List.deletetask)
+        deletebtn.addEventListener('click',to_Do_List.deleteTask)
+
+        // Capturando o icon de completar
+        let chekedbtn = li.firstElementChild
+        chekedbtn.addEventListener('click',to_Do_List.completeTask)
+
+        // Função que ativará a mensagem caso não tenha tarefas
+        to_Do_List.displayMsg()
+    }
+
+    displayMsg(){
+        if(to_Do_List.totalTaks > 0 ){
+            to_Do_List.msg.style.display = 'none'
+        } else{
+            to_Do_List.msg.style.display = 'block'
+        }
     }
 }
 
@@ -38,6 +68,7 @@ button.addEventListener('click',function(e){
 
     // Limpando o campo
     to_Do_List.input.value = ''
+
 })
 
 // Window event
