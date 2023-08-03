b68d739cd88c37e751103ffa35a14ea6941a92a2
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-03 18:13:00 -0300

    Removendo tarefas

--- Projetos/To do List/js/main.js
+++ Projetos/To do List/js/main.js
@@ -5,10 +5,9 @@ class List{
     }
     deletetask(){
         let li = this.parentElement
-        li.remove(li)
+        li.remove()
     }
-    btnClick(){
-        let input = to_Do_List.input.value
+    btnClick(input){
         // Criando elemento li
         let li = document.createElement('li')
         // Modificando o conteúdo interno html do li
@@ -16,20 +15,44 @@ class List{
         // Jogando o li para dentro do ul(Lista)
         to_Do_List.ul.append(li)
 
+        // Capturando icon de remover
         let deletebtn = li.lastElementChild
         deletebtn.addEventListener('click',to_Do_List.deletetask)
     }
 }
 
-// Parandoo envio do formulário
-const form = document.querySelector('form')
-form.addEventListener('submit',function(e){
-    e.preventDefault()
-})
 // Iniciando um novo objeto
 let to_Do_List = new List
 
 // button click
 const button = document.getElementById('click')
-button.addEventListener('click',to_Do_List.btnClick)
+button.addEventListener('click',function(e){
+    // Parando o formulário.
+    e.preventDefault()
 
+    // criando uma variável que armazena o valor do meu input, e adicionando evento de adicionar a tarefa
+    let input = to_Do_List.input.value
+    if(input !== ''){
+        to_Do_List.btnClick(input)
+    }
+
+    // Limpando o campo
+    to_Do_List.input.value = ''
+})
+
+// Window event
+window.addEventListener('keydown',function(e){
+    if(e.key == 'Enter'){
+        // Parando o formulário.
+        e.preventDefault()
+
+        // criando uma variável que armazena o valor do meu input, e adicionando evento de adicionar a tarefa
+        let input = to_Do_List.input.value
+        if(input !== ''){
+            to_Do_List.btnClick(input)
+        }
+
+        // Limpando o campo
+        to_Do_List.input.value = ''
+    }
+})
